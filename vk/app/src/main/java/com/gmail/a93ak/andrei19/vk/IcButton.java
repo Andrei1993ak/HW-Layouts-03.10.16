6d67e8c4ca3a4efbc230dcc904b31884620ce6cc
Andrei Kozlowsky <andrei19.93ak@gmail.com>
2016-10-06 21:28:30 +0300

    HW 05.10 final Add left/bottom icon support for IcButton class. Add new attribete lefticononClickButton for IcButton class

--- vk/app/src/main/java/com/gmail/a93ak/andrei19/vk/IcButton.java
+++ vk/app/src/main/java/com/gmail/a93ak/andrei19/vk/IcButton.java
@@ -5,9 +5,19 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.widget.AppCompatDrawableManager;
 import android.support.v7.widget.TintTypedArray;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.Button;
 
 public class IcButton extends Button {
+
+    private OnClickListener onClickListener;
+    private OnClickListener mineClickListener;
+    private Drawable leftIconOnClick;
+    private Drawable leftIcon;
+    private Drawable topIcon;
+    private Drawable bottomIcon;
+    private Drawable rightIcon;
+
 
     public IcButton(final Context context) {
         super(context, null, R.attr.icButtonStyle);
@@ -29,34 +39,41 @@ public class IcButton extends Button {
     }
 
     private void init(final AttributeSet attrs, final int defStyleAttr) {
-        final TintTypedArray a = TintTypedArray.obtainStyledAttributes(getContext(), attrs, R.styleable.IcButtonStyle, defStyleAttr, 0);
 
+        TintTypedArray a = TintTypedArray.obtainStyledAttributes(getContext(), attrs, R.styleable.IcButtonStyle, defStyleAttr, 0);
         try {
             final AppCompatDrawableManager drawableManager = AppCompatDrawableManager.get();
 
-            Drawable rightIcon = a.getDrawableIfKnown(R.styleable.IcButtonStyle_rightIcon);
+
+            rightIcon = a.getDrawableIfKnown(R.styleable.IcButtonStyle_rightIcon);
             int id = a.getResourceId(R.styleable.IcButtonStyle_rightIcon, -1);
             if (id != -1) {
                 rightIcon = drawableManager.getDrawable(getContext(), id);
             }
 
-            Drawable topIcon = a.getDrawableIfKnown(R.styleable.IcButtonStyle_topIcon);
+            topIcon = a.getDrawableIfKnown(R.styleable.IcButtonStyle_topIcon);
             id = a.getResourceId(R.styleable.IcButtonStyle_topIcon, -1);
             if (id != -1) {
                 topIcon = drawableManager.getDrawable(getContext(), id);
             }
 
-            Drawable leftIcon = a.getDrawableIfKnown(R.styleable.IcButtonStyle_leftIcon);
+            leftIcon = a.getDrawableIfKnown(R.styleable.IcButtonStyle_leftIcon);
             id = a.getResourceId(R.styleable.IcButtonStyle_leftIcon, -1);
             if (id != -1) {
                 leftIcon = drawableManager.getDrawable(getContext(), id);
             }
 
 
-            Drawable bottomIcon = a.getDrawableIfKnown(R.styleable.IcButtonStyle_bottomIcon);
+            bottomIcon = a.getDrawableIfKnown(R.styleable.IcButtonStyle_bottomIcon);
             id = a.getResourceId(R.styleable.IcButtonStyle_bottomIcon, -1);
             if (id != -1) {
                 bottomIcon = drawableManager.getDrawable(getContext(), id);
+            }
+
+            leftIconOnClick = a.getDrawableIfKnown(R.styleable.IcButtonStyle_onClickLeftIcon);
+            id = a.getResourceId(R.styleable.IcButtonStyle_onClickLeftIcon, -1);
+            if (id != -1) {
+                leftIconOnClick = drawableManager.getDrawable(getContext(), id);
             }
 
             setCompoundDrawablesWithIntrinsicBounds(leftIcon, topIcon, rightIcon, bottomIcon);
@@ -64,5 +81,28 @@ public class IcButton extends Button {
         } finally {
             a.recycle();
         }
+
+        mineClickListener = new OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                setCompoundDrawablesWithIntrinsicBounds(leftIconOnClick, null, null, null);
+                if (onClickListener != null) {
+                    onClickListener.onClick(v);
+                }
+            }
+        };
+        setOnClickListener(mineClickListener);
+
     }
+
+    @Override
+    public void setOnClickListener(final OnClickListener l) {
+        if (l.equals(mineClickListener)) {
+            super.setOnClickListener(l);
+        } else {
+            onClickListener = l;
+        }
+    }
+
 }
--- vk/app/src/main/java/com/gmail/a93ak/andrei19/vk/MainActivity.java
+++ vk/app/src/main/java/com/gmail/a93ak/andrei19/vk/MainActivity.java
@@ -2,13 +2,11 @@ package com.gmail.a93ak.andrei19.vk;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -23,18 +21,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-        Display displ = getWindowManager().getDefaultDisplay();
-        int pixelWidth = displ.getWidth();
-        int pixelHigh = displ.getHeight();
-        int size = pixelHigh <= pixelWidth ? pixelHigh : pixelWidth;
+        Point displaySize = new Point();
+        getWindowManager().getDefaultDisplay().getSize(displaySize);
+        int size = displaySize.x <= displaySize.y ? displaySize.x : displaySize.y;
         ImageView imageView2 = (ImageView) findViewById(R.id.image2);
         ImageView imageView3 = (ImageView) findViewById(R.id.image3);
         imageView2.getLayoutParams().width = size;
         imageView3.getLayoutParams().width = size;
-
 
         Bitmap oldBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.foto_andrey);
         Bitmap blurredBitmap = BlurBuilder.blur(this, oldBitmap);
